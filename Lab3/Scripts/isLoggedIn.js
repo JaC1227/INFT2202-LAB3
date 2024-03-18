420cc2ed9f2f0b8daec8323c57056d4607a825b2
JaC1227 <156141013+JaC1227@users.noreply.github.com>
2024-03-18 14:38:43 -0400

    Lab3 - show link in navbar if user is logged in

--- Lab3/Scripts/isLoggedIn.js
+++ Lab3/Scripts/isLoggedIn.js
@@ -4,7 +4,13 @@
     // check if the user is already logged in
     if(sessionStorage.getItem("user"))
     {
-    // redirect to the secure area
-    location.href = "contact-list.html";
+        // redirect to the secure area
+        location.href = "contact-list.html";
+        // user is logged in
+        sessionStorage.setItem("isLoggedIn", true);
+    } else {
+        // user is not logged in
+        sessionStorage.setItem("isLoggedIn", false);
     }
+
 })();
